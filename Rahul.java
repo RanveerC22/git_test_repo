451de78e2ce7a9ae9123e1b0f871ec1c7b8cdd40
Ranveer Ramesh Chavare <ranveerchavare2002@gmail.com>
2023-07-28 20:03:20 +0530

    Added rahul java

---
+++ Rahul.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Rahul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hey this is Rahul, Hello");
+
+	}
+
+}
