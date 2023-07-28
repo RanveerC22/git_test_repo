212144cfe7db0e8712e45693b8df6d2ba7f33f8c
Ranveer Ramesh Chavare <ranveerchavare2002@gmail.com>
2023-07-28 22:13:54 +0530

    fist commit on 3rd file

---
+++ sayinghello.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class sayinghello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Ji i am saying hello");
+		System.out.println("Hello Ji i am saying hello to u");
+
+	}
+
+}
