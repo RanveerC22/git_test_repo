eba33f18bd7c04b099ed73ea5bf421de884e7300
Ranveer Ramesh Chavare <ranveerchavare2002@gmail.com>
2023-07-28 22:30:30 +0530

    main2hello is committed

---
+++ main2hello.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class main2hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello I will be in new branch on github");
+	}
+
+}
